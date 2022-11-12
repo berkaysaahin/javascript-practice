25073d75c6be4d751c0fceb1f53b5ca4c9136abf
berkaysaahin <berkaynihas@gmail.com>
2022-11-12 22:38:26 +0300

    Book-Store-Frontend

---
+++ src/app.js
@@ -0,0 +1,14 @@
+// let showProducts = function(id,...products){
+//     console.log(id)
+//     console.log(products)
+// }
+// //... rest, array halinde parametre verir
+
+// //console.log(typeof showProducts)
+// showProducts(1,"ıy","ew","öğk")
+
+let populations = [1,2,3]
+let [sm,md,hg] = populations
+console.log(sm)
+console.log(md)
+console.log(hg)
